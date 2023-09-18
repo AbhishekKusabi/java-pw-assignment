fd86acda44113fdf081c5116b1645010893a2df4
AbhishekKusabi <122898405+AbhishekKusabi@users.noreply.github.com>
2023-09-18 14:06:12 +0530

    Assignment on Operators

---
+++ Operators.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Operators {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+//        Q1 - Write a program to add 8 to the number x and then divide it by 3. Now, the modulus of the quotient
+//      is taken with 5 and then multiply the resultant value by 5. Display the final result.
+//        int x = 2345;
+//        x+=8;
+//        x/=3;
+//        x%=5;
+//        x*=5;
+//        System.out.println(x);
+
+//        Q2 - Swap two numbers without the use of third variable.
+
+//        int a=4 ,b=10;
+//        a = a+b;
+//        b= a-b;
+//        a=a-b;
+//        System.out.println(a+" "+b);
+
+//        Q3 - Write a program to calculate the sum of the digits of a 3-digit number.
+//        int a = sc.nextInt();
+//        int sum = 0;
+//        while(a>0){
+//            sum+=a%10;
+//            a=a/10;
+//        }
+//            System.out.println(sum);
+
+//        Q4 - Assign values of variables 'a' and 'b' as 55 and 70 respectively and then check if:
+//        i) both the conditions 'a < 50' and 'a < b' are true.
+//        ii) at least one of the conditions 'a < 50' or 'a < b' is true.
+
+//        int a=55;
+//        int b=70;
+//
+//        System.out.println(a<50 && a<b);
+//        System.out.println(a<50 || a<b);
+
+//        Q5 - Find the total number of bits needed to be flipped to convert x to y.
+
+
+    }
+}
